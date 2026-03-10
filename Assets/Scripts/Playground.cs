14ce6c768cd85a6727a1089845aedcfeef3e2676
JMC-Academy <bvotano@myjmc.edu.au>
2026-03-10 18:15:15 +1100

    tweening and Script fun

---
+++ Assets/Scripts/Playground.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playground : MonoBehaviour
+{
+    public GameObject cube;
+    public List<string> names;
+    public BV.Range spawnRate;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            cube.gameObject.GetComponent<Renderer>().material.color = ColorX.GetRandomColor();
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            ListX.ShuffleList(names);
+            Debug.Log(names[0]);
+        }
+    }
+}
---
+++ Assets/Scripts/TweenFun.cs
@@ -0,0 +1,93 @@
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class TweenFun : BehaviourX
+{
+    public enum Direction { North, East, South, West}
+    [SerializeField] private Transform player;
+    [SerializeField] private float moveDistance = 5;
+    [SerializeField] private float moveDuration = 1;
+    [SerializeField] private float shakeStrength = 0.4f;
+    [SerializeField] private Ease moveEase;
+
+    [SerializeField] private bool gridMovement;
+    private bool isMoving;
+
+    [Header("UI")]
+    [SerializeField] private CanvasGroup fadeCanvas;
+    [SerializeField] private float fadeTime = 2f;
+    [SerializeField] private Ease fadeEase;
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private int scoreBonus = 100;
+    [SerializeField] private float scoreTweenTime = 1f;
+    [SerializeField] private Ease scoreEase;
+    private int score;
+
+    private void Start()
+    {
+        fadeCanvas.alpha = 1;
+        fadeCanvas.DOFade(0, fadeTime).SetEase(fadeEase);
+
+        ExecuteAfterSeconds(2, () => print("You are great"));
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+            MovePlayer(Direction.North);
+        if (Input.GetKeyDown(KeyCode.D))
+            MovePlayer(Direction.East);
+        if (Input.GetKeyDown(KeyCode.S))
+            MovePlayer(Direction.South);
+        if (Input.GetKeyDown(KeyCode.A))
+            MovePlayer(Direction.West);
+    }
+
+    private void MovePlayer(Direction _direction)
+    {
+        if (isMoving && gridMovement)
+            return;
+
+        isMoving = true;
+
+        switch (_direction)
+        {
+            case Direction.North:
+                player.DOLocalMoveZ(player.localPosition.z + moveDistance, moveDuration)
+                    .SetEase(moveEase)
+                    .OnComplete(TweenComplete);
+                break;
+            case Direction.East:
+                player.DOLocalMoveX(player.localPosition.x + moveDistance, moveDuration)
+                    .SetEase(moveEase)
+                    .OnComplete(TweenComplete);
+                break;
+            case Direction.South:
+                player.DOLocalMoveZ(player.localPosition.z - moveDistance, moveDuration)
+                    .SetEase(moveEase)
+                    .OnComplete(TweenComplete);
+                break;
+            case Direction.West:
+                player.DOLocalMoveX(player.localPosition.x - moveDistance, moveDuration)
+                    .SetEase(moveEase)
+                    .OnComplete(TweenComplete);
+                break;
+        }
+        ChangeColor();
+    }
+
+    private void TweenComplete()
+    {
+        isMoving = false;
+        ScreenShake();
+        IncreaseScore();
+    }
+    private void ScreenShake() => Camera.main.DOShakePosition(moveDuration / 2, shakeStrength);
+    private void ChangeColor() => player.GetComponent<Renderer>().material.DOColor(ColorX.GetRandomColor(), moveDuration);
+    private void IncreaseScore()
+    {
+        TweenX.TweenNumbers(scoreText, score, score + scoreBonus, scoreTweenTime, scoreEase, "F0");
+        score = score + scoreBonus;
+    }
+}
